aa731ad38cc4e9e52276563375e19d9bad352790
Tim Berglund <tim.berglund@datastax.com>
2015-07-29 08:18:22 -0600

    Added exercise anchors

--- src/main/groovy/com/datastax/curriculum/gradle/tasks/CourseTask.groovy
+++ src/main/groovy/com/datastax/curriculum/gradle/tasks/CourseTask.groovy
@@ -106,10 +106,12 @@ class CourseTask extends DefaultTask {
       vertexList.each { vertex ->
         def vertexExercisesFile = "${curriculumRootDir}/${vertex}/src/exercises.adoc"
         if(project.file(vertexExercisesFile).exists()) {
-          writer.println ":exercise_number: ${exerciseNumber++}"
+          writer.println ":exercise_number: ${exerciseNumber}"
           writer.println ":image_path: ../../images/${vertex}"
+          writer.println "[[EXERCISE-${exerciseNumber}]]"
           writer.println "include::${curriculumRootDir}/${vertex}/src/exercises.adoc[]"
           writer.println ''
+          exerciseNumber++;
         }
       }
       writer.flush()
