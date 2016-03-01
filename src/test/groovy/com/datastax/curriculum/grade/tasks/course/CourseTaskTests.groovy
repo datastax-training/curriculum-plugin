329302d029c130d7ec100182f1e912b120512ae9
Tim Berglund <tim.berglund@datastax.com>
2016-03-01 05:31:48 -0500

    Added unit test infrastructure

---
+++ src/test/groovy/com/datastax/curriculum/grade/tasks/course/CourseTaskTests.groovy
@@ -0,0 +1,13 @@
+package com.datastax.curriculum.grade.tasks.course
+
+import org.junit.Test
+import static org.junit.Assert.*
+
+
+class CourseTaskTests {
+
+  @Test
+  void testVertexList() {
+    assertTrue(true)
+  }
+}
